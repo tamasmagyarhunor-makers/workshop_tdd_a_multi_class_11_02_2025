40a89305fc0cb4b4775fe4d6611714c9216bfb4e
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-02-11 12:00:46 +0000

    Update tests

--- tests/test_box.py
+++ tests/test_box.py
@@ -1,5 +1,4 @@
 from lib.box import Box
-from lib.item import Item
 from unittest.mock import Mock
 
 def test_box_instantiates():
@@ -23,20 +22,20 @@ def test_get_store_returns_store():
 
 def test_box_when_item_added_it_appears_in_the_store():
     box = Box()
-    item = Item('baseball bat', 'wooden')
+    item = Mock()
     box.add_item(item)
 
     assert box.get_store() == [item]
 
 def test_box_collects_details_of_items():
     box = Box()
-    item = Mock()
-    item_2 = Mock()
-    item.get_name.return_value = "I am a(n) amazing baseball bat"
-    item_2.get_name.return_value = "I am a(n) amazing barbie"
+    mock = Mock()
+    mock_2 = Mock()
+    mock.get_name.return_value = "I am a(n) amazing baseball bat"
+    mock_2.get_name.return_value = "I am a(n) amazing barbie"
 
-    box.add_item(item)
-    box.add_item(item_2)
+    box.add_item(mock)
+    box.add_item(mock_2)
 
     item_descriptions = box.get_items_descriptions()
 
